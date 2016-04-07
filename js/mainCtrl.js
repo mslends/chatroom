b5eecd04edeece715fbb24bca4fcea77385838ad
Mike Spencer <mslends@gmail.com>
2016-04-07 09:47:04 -0600

    Finished chat room.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,21 +2,34 @@ var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
-
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
+$scope.getParseData = function() {
+  parseService.getData().then(function(response) {
+    $scope.messages = response.results;
+    console.log($scope.message);
+  })
+}
+$scope.getParseData();
+  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to
   //your controllers $scope as messages ($scope.messages)
 
-
+$scope.postData = function() {
+  parseService.postData($scope.message).then(function(response) {
+    $scope.messages = ' ';
+    $scope.getParseData();
+  })
+}
 
   //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
-
-
+$scope.sort = true;
+$scope.sortDirection = function() {
+  $scope.sort = !$scope.sort;
+}
 
 
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
+  setInterval(function(){
+    $scope.getParseData();
+  }, 1500)
 })
